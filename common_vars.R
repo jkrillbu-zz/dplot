d46f2d5ccf37e806a2e4440636d949dbaf9c843d
Mike Burger <mburger@broadinstitute.org>
2016-01-25 13:49:35 -0500

    Load dataset filenames from common_vars file

--- common_vars.R
+++ common_vars.R
@@ -11,3 +11,26 @@ axis_vars <- c(
   "CCLE CN" = "CN",
   "CTD2 AUC" = "CTD2"
 )
+
+taiga_id <- list(gene.rpkm="013458ff-bb36-4fd7-ac87-dcc04b15174e",
+                 demeter="966b70bf-c414-43ad-97ec-1a8b50f91ed6",
+                 demeter.ranks="6ce39e1f-b1e0-4085-b644-c8604f6e8fe9",
+                 gecko="886897b5-48b1-47c5-98bb-3c40e2c5cdde",
+                 gecko.ranks="e0136d0d-ad4b-40c3-ba9c-820d1a26797b",
+                 gecko.segment="1cb9fe68-b625-4af5-8dbe-e19ff4223764",
+                 avana="2949fd33-f417-42ea-ba5b-3a100e2b39ec",
+                 avana.ranks="3587811b-8d6f-4481-b616-391c5bd3aa50",
+                 avana.segment="be462b17-aafa-476f-8cc8-5751b5eecc99",
+                 CN="20f80986-b2a9-4ced-948b-3fe0bc0522d7",
+                 CTD2="55098e9e-7807-4df6-9c14-bbced64c06bd",
+                 in.sets="2256678c-8fe0-4f59-a7d7-a2c7c716bf83",
+                 in.sets.cols="cbd33823-2951-4070-adb8-b095dab9a628",
+                 dna.missense.inframe="047c1688-1c61-405c-8990-b3bfb9e0e2a3",
+                 dna.damage="f9b17f28-0b4c-4e60-89a8-ff9501ab8b2d",
+                 dna.hotspot="8a771b60-ef2d-40ff-ad2d-e2a17d42c4b4",
+                 rna.missense.inframe="92c22ffa-6204-4808-a767-893011d54f13",
+                 rna.damage="f3fd436d-7642-4563-bc9e-28dc32457b7e",
+                 rna.hotspot="1d762322-6abb-4cd0-ac23-03d8369492b6",
+                 lineage.matrix="ddbb3f5a-62cd-4d0f-ad05-27185cc18772"
+                 
+)
--- server.R
+++ server.R
@@ -13,41 +13,23 @@ library(dplyr)
 source("hdf5_utils.R")
 if (FALSE) library(RSQLite)
 
-taiga_id <- list(gene.rpkm="013458ff-bb36-4fd7-ac87-dcc04b15174e",
-                 demeter="966b70bf-c414-43ad-97ec-1a8b50f91ed6",
-                 demeter.ranks="6ce39e1f-b1e0-4085-b644-c8604f6e8fe9",
-                 gecko="886897b5-48b1-47c5-98bb-3c40e2c5cdde",
-                 gecko.ranks="e0136d0d-ad4b-40c3-ba9c-820d1a26797b",
-                 gecko.segment="1cb9fe68-b625-4af5-8dbe-e19ff4223764",
-                 avana="2949fd33-f417-42ea-ba5b-3a100e2b39ec",
-                 avana.ranks="3587811b-8d6f-4481-b616-391c5bd3aa50",
-                 avana.segment="be462b17-aafa-476f-8cc8-5751b5eecc99",
-                 CN="20f80986-b2a9-4ced-948b-3fe0bc0522d7",
-                 CTD2="55098e9e-7807-4df6-9c14-bbced64c06bd",
-                 in.sets="2256678c-8fe0-4f59-a7d7-a2c7c716bf83",
-                 in.sets.cols="cbd33823-2951-4070-adb8-b095dab9a628",
-                 dna.missense.inframe="047c1688-1c61-405c-8990-b3bfb9e0e2a3",
-                 dna.damage="f9b17f28-0b4c-4e60-89a8-ff9501ab8b2d",
-                 dna.hotspot="8a771b60-ef2d-40ff-ad2d-e2a17d42c4b4",
-                 rna.missense.inframe="92c22ffa-6204-4808-a767-893011d54f13",
-                 rna.damage="f3fd436d-7642-4563-bc9e-28dc32457b7e",
-                 rna.hotspot="1d762322-6abb-4cd0-ac23-03d8369492b6",
-                 lineage.matrix="ddbb3f5a-62cd-4d0f-ad05-27185cc18772"
-                 
-  )
-
 mut_combo <- list(dna.mut=paste(taiga_id[["dna.missense.inframe"]],taiga_id[["dna.damage"]],taiga_id[["dna.hotspot"]],sep=","),
                    rna.mut=paste(taiga_id[["rna.missense.inframe"]],taiga_id[["rna.damage"]],taiga_id[["rna.hotspot"]],sep=","))
 
 sample_info <- data.frame(ID=get_values(taiga_id[["in.sets"]],"CCLE_ID",hdf5_dir),lineage=get_values(taiga_id[["in.sets"]],"lineage",hdf5_dir))
 
-default_features <- list(demeter="BRAF",demeter.ranks="BRAF",gecko="BRAF_1_001111",gecko.ranks="BRAF_1_001111",gecko.segment="BRAF",avana="BRAF_1_1111",avana.ranks="BRAF_1_1111",avana.segment="BRAF",gene.rpkm="BRAF",CN="BRAF",CTD2="nutlin-3")
+random_default <- function(dataset){
+  cols <- get_colnames(taiga_id[[dataset]],hdf5_dir)
+  def_feature <- cols[sample.int(length(cols),1)]
+  return(def_feature)
+}
+
+default_features <- list()
+for (dataset in axis_vars){
+  default_features[[dataset]] <- random_default(dataset)
+}
 
 shinyServer(function(input, output, session) {
-   
-  #x_dataset <- reactive({
-  #  input$x_dataset
-  #})
     
   observe({
     x_dataset <- input$x_dataset
